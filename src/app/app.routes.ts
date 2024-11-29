a66de04f1f60032315117214a70e3d175356b805
Aayush Sharma <aayushnsunny78@gmail.com>
2024-11-29 15:03:00 +0530

    final changes and complete app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contactus', component: ContactusComponent }
+];
