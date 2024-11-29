2a6ac9d41746c28fb2d87fb8591a02acea04897e
Aayush Sharma <aayushnsunny78@gmail.com>
2024-11-29 10:48:13 +0530

    added condition if todods are empty

--- src/app/insert-todo/insert-todo.component.ts
+++ src/app/insert-todo/insert-todo.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../../Model/Todo';
 
 @Component({
   selector: 'app-insert-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './insert-todo.component.html',
   styleUrl: './insert-todo.component.css'
 })
 
 export class InsertTodoComponent {
+
+  @Input() todo: Todo;
+  @Output() AddTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor() {
+    this.todo = new Todo(1, "", "", true);
+  }
+
+  srno!: number;
+  title!: string;
+  desc!: string;
+  isactive!: boolean;
+
+  OnSubmit() {
+
+    var todo: Todo = {
+      srno: this.srno,
+      title: this.title,
+      desc: this.desc,
+      isactive: true
+    };
+
+    this.AddTodo.emit(todo);
+
+  }
 
 }
